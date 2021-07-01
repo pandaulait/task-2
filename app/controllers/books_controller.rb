78fa7ff55f1209a69bc9b2ce219a342abfc739bf
pandaulait <show.hey.panda@gmail.com>
2021-07-01 08:56:39 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,20 @@
 class BooksController < ApplicationController
-
+  before_action :ensure_correct_user, only: [:update,:edit]
   def show
     @book = Book.find(params[:id])
+    @newbook = Book.new
   end
 
   def index
     @books = Book.all
+    @newbook = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
+      redirect_to book_path(@newbook), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -34,16 +36,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
+  end
 end
